51e6792116b1f0ee5764ab2d5fe1a30735005a47
Siddharth Salunkhe <siddharth.salunkhe11@gmail.com>
2021-05-03 16:42:57 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello there. General Kenobi
+          I am the senate
         </p>
         <a
           className="App-link"
